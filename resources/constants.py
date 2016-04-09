1b56eb39ac06a76c6eab6614db2ef1693c542dcb
Herik Webb <herikwebb@gmail.com>
2016-04-09 15:06:46 -0400

    adding weller mystery bottles

--- resources/constants.py
+++ resources/constants.py
@@ -25,6 +25,10 @@ products = {
     'Buffalo Trace 1.75L': '018008',
     'EH Taylor Seasoned Wood': '021603',
     'Yellowstone 105 7yr': '022246',
+    'WL Weller Mystery 1': '022036',
+    'WL Weller Mystery 2': '021976',
+    'WL Weller Mystery 3': '950044',
+    'WL Weller Mystery 4': '950234'
 }
 
 # products = {'Ancient Age 10y 1.75L': '016538',
