a1cac88d0fb04a8e0e3f601aff4b436bc90a557d
Herik Webb <herikwebb@gmail.com>
2016-12-05 23:59:52 -0500

    commenting out lottery/reserve items

--- resources/constants.py
+++ resources/constants.py
@@ -1,48 +1,31 @@
 products = {
-    'Old Forester Birthday': '000517',
+    #'Old Forester Birthday': '000517',
     'Four Roses LE Small Batch': '018361',
     'Four Roses LE Small Batch Barrel': '018365',
     'Lost Prophet': '019456',
     'Elijah Craig 18y': '017920',
     'Elijah Craig 21y': '017923',
     'Elijah Craig 23y': '017925',
-    'Pappy Van Winkle 20yr': '017756',
-    'Pappy Van Winkle 15yr': '020150',
-    'Van Winkle Special Reserve 12yr': '021906',
-    'Old Rip Van Winkle 10yr': '020140',
-    'George T. Stagg': '018416',
-    'Eagle Rare 17yr': '017756',
-    'Sazerac Rye 18yr': '027096',
-    'William Larue Weller': '022086',
+    #'Pappy Van Winkle 20yr': '017756',
+    #'Pappy Van Winkle 15yr': '020150',
+    #'Van Winkle Special Reserve 12yr': '021906',
+    #'Old Rip Van Winkle 10yr': '020140',
+    #'George T. Stagg': '018416',
+    #'Eagle Rare 17yr': '017756',
+    #'Sazerac Rye 18yr': '027096',
+    #'William Larue Weller': '022086',
+    #'EH Taylor Small Batch': '021602',
+    #'EH Taylor Single Barrel': '952796',
+    #'EH Taylor Straight Rye': '027101',
+    #'EH Taylor Seasoned Wood': '021603',
     'EH Taylor Barrel Proof': '021600',
     'WL Weller Antique': '022036',
     'WL Weller 12 Year': '950044',
     'WL Weller Special Reserve': '950234',
     'Sazerac Rye 6yr': '027100',
     'Buffalo Trace Experimental': '0953565'
+    #'Stagg Jr.': '021540',
+    #'Knob Creek LE 2001': '019240',
+    #'Elmer T. Lee': '017946',
+    #'Elijah Craig Barrel Proof': '017914',
 }
-
-# products = {'Ancient Age 10y 1.75L': '016538',
-#             'Ancient Age 10y': '016537'}
-
-# products = {'Elijah Craig': '017916',
-#             'Elijah Craig 12y': '017919'}
-
-# products = {'Buffalo Trace 1.75L': '018008'}
-
-# products = {'Sazerac Rye 6yr': '027100'}
-
-# products = {'Yellowstone 105 7yr': '022246',
-#             'EH Taylor Small Batch': '021602',
-#             'EH Taylor Single Barrel': '952796',
-#             'EH Taylor Straight Rye': '027101',
-#             'EH Taylor Seasoned Wood': '021603',
-#             'Old Forester Birthday 2014': '000811',
-#             'Blood Oath': '016860',
-#             'WL Weller Centennial': '021976',
-# }
-
-#    'Stagg Jr.': '021540',
-#    'Knob Creek LE 2001': '019240',
-#    'Elmer T. Lee': '017946',
-#    'Elijah Craig Barrel Proof': '017914',
